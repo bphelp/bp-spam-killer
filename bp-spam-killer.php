dc4d906217bb57734dae87577f532ff0488b6443
Ben Jones <icompugeek@gmail.com>
2013-05-17 16:07:54 -0400

    Initial Release

--- bp-spam-killer.php
+++ bp-spam-killer.php
@@ -10,20 +10,21 @@ if ( !function_exists( 'bp_core_install' ) ) {
 	}
 }
 
-
 function bp_spam_killer_install_buddypress_notice() {
 	echo '<div id="message" class="error fade"><p style="line-height: 150%">';
 	_e('<strong>BP Spam Killer</strong></a> requires the BuddyPress plugin to work. Please <a href="http://buddypress.org/download">install BuddyPress</a> first, or <a href="plugins.php">deactivate BP Spam Killer</a>.');
 	echo '</p></div>';
 }
 
+/*Add HoneyPot Hidden Field*/
 function bphelp_add_honeypot() {
 	echo '<div style="display: none;">';
 	echo '<input type="text" name="are_you_a_spammer" id="rejected" />';
 	echo '</div>';
 }
 add_action('bp_after_signup_profile_fields','bphelp_add_honeypot');
-	
+
+/*Redirect To Spam Prevention Message If Supected To Be A Spammer*/	
 function bphelp_check_honeypot() {
 	if (!empty($_POST['system55'])) {
 			global $bp;
